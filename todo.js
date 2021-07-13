48c217628dd9bd9e5b7cfd7da15177f16335fd81
nandani-patel <nandanip271@gmail.com>
2021-07-14 00:23:59 +0530

    delete task button

--- todo.js
+++ todo.js
@@ -27,25 +27,23 @@ let add = document.getElementById("add");
           alert("cant append empty item to the list");
         }    
         else{
-            info.innerHTML += '<div class="des">' + input1.value + '</div>'
-            info.innerHTML += '<div class="deadline">' + input2.value + '</div>'
-            info.innerHTML += '<div class="hours">' + input3.value + '</div>'
+            info.innerHTML += '<div class="des">' + input1.value + '<span id="deadline">' + input2.value + '</span> <span id="hours"> ' + input3.value + '</span> <button onclick="document.body.removeChild(this.parentNode)" id = "del" style="float: right;" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg></button> </div>'
+
             store();
             input1.value = " ";
             input2.value = " ";
             input3.value = " ";
+            
             modal.style.display = "none";   
         }  
     },false)
-    info.addEventListener('click',function(e){
-      var t1 = e.target;
-      // t1.parentNode.removeChild(t1);
-    store();
-    },false)
+  $(document).on('click', ':button', function() {
+      $(this).parent().remove();
+      store();
+  });
     function store() {
       localStorage.setItem("testKey1", JSON.stringify(info.innerHTML));
-    }
-    let del = document.getElementById('del');
+    } 
     function getValues() {
       var storedValues1 = JSON.parse(localStorage.getItem("testKey1"));
       info.innerHTML = storedValues1; 
