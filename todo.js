85cb2145157eea6afce3464745a29e3260266213
nandani-patel <nandanip271@gmail.com>
2021-07-15 00:25:29 +0530

    changes

--- todo.js
+++ todo.js
@@ -25,10 +25,21 @@ let add = document.getElementById("add");
         }    
         else{
             info.innerHTML += `<div class="task-card">
-            <div class="title">${input1.value}</div>
-            <div class="descr"><textarea name="input" id="" cols="30" rows="10"></textarea></div>
-            <div class="deadline">${input2.value}</div><div class="hours">${input3.value}</div>
-            <button id="del" onclick="document.body.removeChild(this.parentNode)" id = "del" style="float: right;" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg></button>
+            <div class="title">
+            ${input1.value}
+            </div>
+            <div class="descr">
+            <textarea name="input" heigth="40px" width="100px"></textarea>
+            </div>
+            <div class="deadline">
+            ${input2.value}
+            </div>
+            <div class="hours">
+            ${input3.value}
+            </div>
+            <button id="del"  id = "del" style="float: right;" >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>
+            </button>
           </div>`
             store();
             input1.value = " ";
@@ -37,7 +48,7 @@ let add = document.getElementById("add");
             modal.style.display = "none";   
         }  
     },false)
-  $(document).on('click', ':del', function() {
+  $(document).on('click', ':button', function() {
       $(this).parent().remove();
       store();
   });
