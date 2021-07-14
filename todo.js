71082a7e4bfc26c3beeae46dc00344af811bafb2
nandani-patel <nandanip271@gmail.com>
2021-07-15 01:51:18 +0530

    3 bugs fix

--- todo.js
+++ todo.js
@@ -1,10 +1,10 @@
 var modal = document.getElementById("Modal");
 let b1 = document.getElementById("b1"); 
+var t1 = document.getElementById("t");
 
 b1.onclick = function() {
   modal.style.display = "block";
 }
-
 
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
@@ -16,7 +16,6 @@ let add = document.getElementById("add");
 
 
 (function(){
-
     add.addEventListener('click',function(e){
       e.preventDefault();
         if(input1.value === ''){
@@ -34,34 +33,34 @@ let add = document.getElementById("add");
         else{ 
             info.innerHTML += `<div class="task-card">
             <div class="title">
-            ${input1.value}
-            </div>
-            <div class="descr">
-            <textarea name="input" heigth="40px" width="100px"></textarea>
-            </div>
+            ${input1.value} 
+            </div> 
+            <button onclick="location.reload();" id="del" style="float: right;" >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>
+            </button>
             <div class="deadline">
             ${input2.value}
             </div>
             <div class="hours">
             ${input3.value}
             </div>
-            <button id="del"  id = "del" style="float: right;" >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>
-            </button>
+            <div class="descr">
+            <textarea name="input" heigth="100px" width="200px"></textarea>
+            </div>
           </div>`
             store();
             input1.value = " ";
             input2.value = " ";
             input3.value = " ";
             modal.style.display = "none";
-        }  
+        }
+        location.reload();  
     },false)
 
   $(document).on('click', ':button', function() {
       $(this).parent().remove();
       store();
   });
-
     function store() {
       localStorage.setItem("testKey1", JSON.stringify(info.innerHTML));
     } 
@@ -79,4 +78,6 @@ let close = document.getElementById("close");
 
 close.addEventListener('click',function(closemodal){
   modal.style.display = "none";
+  location.reload();
 });
+
