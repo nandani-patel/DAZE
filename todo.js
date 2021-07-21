08eb8c66dbf432e08c393081f913d3690be75815
nandani-patel <82666408+nandani-patel@users.noreply.github.com>
2021-07-21 23:16:27 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -56,8 +56,7 @@ function store() {
                         <div class="descr">
                         <textarea name="input" heigth="100px" width="200px"> ENTER DETAILS/NOTES/IMPORTANT LINKS.....</textarea>
                         </div>
-                        <button onclick="$(this).parent().remove();
-                        store();" id="del" style="float: right; margin-right: 20px" >
+                        <button  id="del" style="float: right; margin-right: 20px" >
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>
                         </button> `
                        
@@ -71,10 +70,10 @@ function store() {
           location.reload();  
       },false)
 
-  // $(document).on('click', ':button', function() {
-  //     $(this).parent().remove();
-  //     store();
-  // });
+  $(document).on('click', ':button', function() {
+      $(this).parent().remove();
+      store();
+  });
     function store() {
       localStorage.setItem("testKey1", JSON.stringify(info.innerHTML));
     } 
